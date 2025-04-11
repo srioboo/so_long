62c7d10552e6763a76cd489b1bcf2e867cd73e24
srioboo <salrio74@gmail.com>
2025-04-11 22:20:00 +0200

    fix: fix movement

--- so_player.c
+++ so_player.c
@@ -6,11 +6,13 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 23:27:53 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/07 20:25:51 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/11 22:18:39 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+#define STEP 64;
 
 static mlx_image_t		*img; // TODO - is this allowed?
 
@@ -34,30 +36,33 @@ int	draw_player(mlx_t *mlx)
 	return (result);
 }
 
-/* TODO - enable moviment */
-// static void	move(mlx_t *mlx, mlx_image_t *img, int amount_x, int amount_y)
 static void	move(int amount_x, int amount_y)
 {
-	//mlx_image_to_window(mlx, img, 800 + amount_x, 100 + amount_y);
-	printf("%d %d %d", img->instances[0].x , amount_x, amount_y);
-	// img->instances[0].x += amount_x;
-	// img->instances[0].y += amount_y;
+	// mlx_image_to_window(mlx, img, 800 + amount_x, 100 + amount_y);
+	
+	// printf("%d %d %d", img->instances[0].x , amount_x, amount_y);
+	printf(" %d %d", amount_x, amount_y);
+	img->instances[0].x += amount_x;
+	img->instances[0].y += amount_y;
 }
 
 void	move_player(mlx_key_data_t keydata, void *param)
 {
 	mlx_t	*mlx;
+	int		step;
 
 	mlx = param;
-	printf("key: %d\n", keydata.key);
+	step = 64;
+	// printf("key: %d %p\n", keydata.key, param);
 	if ((keydata.key == MLX_KEY_D) || (keydata.key == MLX_KEY_RIGHT))
-		move(5, 0);
+		move(step, 0);
 	if ((keydata.key == MLX_KEY_A) || (keydata.key == MLX_KEY_LEFT))
-		move(5, 0);
+		move(-step, 0);
 	if ((keydata.key == MLX_KEY_W) || (keydata.key == MLX_KEY_UP))
-		move(0, 5);
+		move(0, -step);
 	if ((keydata.key == MLX_KEY_S) || (keydata.key == MLX_KEY_DOWN))
-		move(0, 5);
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		move(0, step);
+	//if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE)) // This doesnt work
+	if ((keydata.key == MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 }
