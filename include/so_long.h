54bc70c6660dc2e7d43dfa7c76daacc3871c7b56
srioboo <salrio74@gmail.com>
2025-04-26 20:41:03 +0200

    feat: add validation c file

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:05:25 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/26 16:02:06 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/26 20:25:53 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@
 
 // Image size
 # define IMG_SIZE 64
+
+# define TRUE 1
+# define FALSE 0
 
 /* TODO - delete
 typedef struct s_vars
@@ -72,19 +75,13 @@ typedef struct s_game_data
 }	t_game_data;
 
 /* ************************************************************************** */
-/* DRAW                                                                      */
+/* MAP                                                                      */
 /* ************************************************************************** */
 
 /**
- * @brief Draw a pixel
+ * @brief Validate map is correct
  */
-void		draw_pixel(mlx_image_t *img, t_map_pos pos, int color);
-
-/**
- * @brief Draw a line
- */
-void		draw_line(mlx_image_t *img,
-				t_map_pos pos_ini, t_map_pos pos_end, int color);
+int		validate_map(void);
 
 /* ************************************************************************** */
 /* SCENE                                                                      */
--- so_validate.c
+++ so_validate.c
@@ -1,40 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_draw.c                                          :+:      :+:    :+:   */
+/*   so_validate.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 23:27:53 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/20 17:02:20 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/26 20:39:43 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	draw_pixel(mlx_image_t *img, t_map_pos pos, int color)
+int	validate_map(void)
 {
-	mlx_put_pixel(img, pos.x, pos.y, color);
-}
+	int	is_valid;
 
-void	draw_rectangle(int keycode)
-{
-	ft_printf("draw rectable: %d\n", keycode);
-}
-
-void	draw_square(int keycode)
-{
-	ft_printf("draw square: %d\n", keycode);
-}
-
-void	draw_line(mlx_image_t *img,
-	t_map_pos pos_ini, t_map_pos pos_end, int color)
-{
-	while (pos_ini.x < pos_end.x && pos_ini.x < pos_end.y)
-		mlx_put_pixel(img, pos_ini.x, pos_end.y, color);
-}
-
-void	draw_circle(int keycode)
-{
-	ft_printf("draw circle: %d\n", keycode);
+	is_valid = TRUE;
+	return (is_valid);
 }
