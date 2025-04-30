577570d96b6178b1e646719437077abbaddcc3a1
srioboo <salrio74@gmail.com>
2025-04-30 07:54:52 +0200

    docs: add documentation to the header file

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:05:25 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/04/29 22:13:38 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/04/30 07:54:10 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,9 +77,19 @@ typedef struct s_game_data
 
 /**
  * @brief Validate map is correct
+ * 
+ * @details Validate map and, if correct, return height
+ * @param game_map struct with game data
+ * @return if correct resturn height of the map
  */
 int			validate_map(char *game_map);
 
+/**
+ * @brief load a map
+ * 
+ * @param game_map struct with game data
+ * @return file descriptor of the map
+ */
 int			load_map(char *game_map);
 
 /* ************************************************************************** */
@@ -88,6 +98,9 @@ int			load_map(char *game_map);
 
 /**
  * @brief Get the map
+ * 
+ * @param game_map struct with game data
+ * @return the map struct with player data
  */
 t_map		*get_map(char *game_map);
 
@@ -95,10 +108,10 @@ t_map		*get_map(char *game_map);
  * @brief Draw the scene
  * 
  * @details Draw the scene with the images
+ * @param game_map struct with game data
+ * @return 0 or 1 if the scene has or not errors
  */
 int			draw_scene(t_game_data *game_data);
-
-int			redraw_scene(t_game_data *game_data);
 
 /* ************************************************************************** */
 /* PLAYER                                                                     */
@@ -108,9 +121,18 @@ int			redraw_scene(t_game_data *game_data);
  * @brief Draw the player
  * 
  * @details Draw the player with the images
+ * @param game_map struct with game data
+ * @return the player img
  */
-mlx_image_t	*draw_player(t_game_data *data);
+mlx_image_t	*draw_player(t_game_data *game_data);
 
+/**
+ * @brief Move the player
+ * 
+ * @param game_map struct with game data
+ * @param steps_x steps moving in x
+ * @param steps_y steps moving in y
+ */
 void		move_player(t_game_data *game_data,
 				int steps_x, int steps_y);
 
@@ -118,6 +140,11 @@ void		move_player(t_game_data *game_data,
 /* GAME MANAGEMENT                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief close clean the windows
+ * 
+ * @param game_data
+ */
 void		close_window(t_game_data *game_data);
 
 /**
@@ -125,13 +152,33 @@ void		close_window(t_game_data *game_data);
  */
 void		error(void);
 
+/**
+ * @brief get a image
+ * 
+ * @param mlx
+ * @param relative_path in witch the image can be found
+ * 
+ * @return and image
+ */
 mlx_image_t	*get_image(mlx_t *mlx, char *relative_path);
 
+/**
+ * @brief test if is posible to move
+ * 
+ * @param game data
+ * @param current_x position
+ * @param current_y position
+ * 
+ * @return 0 if can't move 1 if can
+ */
 int			is_move_posible(t_game_data *game_data,
 				int current_x, int current_y);
 
 /**
  * @brief Move the player
+ * 
+ * @param keydata with
+ * @param param, generally game data
  */
 void		process_moves(mlx_key_data_t keydata, void *param);
 
@@ -139,21 +186,75 @@ void		process_moves(mlx_key_data_t keydata, void *param);
 /* OBJECTS                                                                    */
 /* ************************************************************************** */
 
+/**
+ * @brief draw fish
+ * 
+ * @param game_data
+ * @param type 1, 0, E, C, P
+ * @param x position
+ * @param y position
+ * 
+ * @return 1 if is draw 0 if wasn't posible for son error
+ */
 int			draw_fish(t_game_data *game_data, char type, int x, int y);
 
+/**
+ * @brief delete fish
+ * 
+ * @param game_data
+ * @param x position
+ * @param y position
+ * 
+ */
 void		delete_fish_instance(t_game_data *game_data, int x, int y);
 
 /* ************************************************************************** */
 /* OBJECTS                                                                    */
 /* ************************************************************************** */
 
+/**
+ * @brief draw wall
+ * 
+ * @param game_data
+ * @param type 1, 0, E, C, P
+ * @param x position
+ * @param y position
+ * 
+ * @return 1 if is draw 0 if wasn't posible for son error
+ */
 int			draw_wall(t_game_data *game_data, char type, int x, int y);
 
+/**
+ * @brief draw ocean
+ * 
+ * @param game_data
+ * @param type 1, 0, E, C, P
+ * @param x position
+ * @param y position
+ * 
+ * @return 1 if is draw 0 if wasn't posible for son error
+ */
 int			draw_ocean(t_game_data *game_data, char type, int x, int y);
 
+/**
+ * @brief draw exit
+ * 
+ * @param game_data
+ * @param type 1, 0, E, C, P
+ * @param x position
+ * @param y position
+ * 
+ * @return 1 if is draw 0 if wasn't posible for son error
+ */
 int			draw_exit(t_game_data *game_data, char type, int x, int y);
 
+/**
+ * @brief get all images
+ * 
+ * @param game_data
+ * 
+ * @return game_data
+ */
 t_game_data	*get_objects_images(t_game_data *game_data);
-// int			redraw_fish(t_game_data *game_data, char *line, int y);
 
 #endif
