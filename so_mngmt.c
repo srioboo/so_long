3c19fb00c6f75af013797ac4f3c6972c1d1e8a85
srioboo <salrio74@gmail.com>
2025-05-03 23:03:44 +0200

    fix: avoid errors in makefile

--- so_mngmt.c
+++ so_mngmt.c
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 23:27:53 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/05/03 22:52:16 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/05/03 23:02:12 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,19 @@ void	close_game(t_game_data *game_data)
 	//mlx_delete_image(game_data->mlx, game_data->wall_img);
 	//mlx_delete_image(game_data->mlx, game_data->ocean_img);
 
-	if (game_data->exit_img)
-		free(game_data->exit_img);
-	if (game_data->fish_img)
-		free(game_data->fish_img);
-	if (game_data->wall_img)
-		free(game_data->wall_img);
-	if (game_data->ocean_img)
-		free(game_data->ocean_img);
-	if (game_data->player_img)
-		free(game_data->player_img);
+	// if (game_data->exit_img)
+	// 	free(game_data->exit_img);
+	// if (game_data->fish_img)
+	// 	free(game_data->fish_img);
+	// if (game_data->wall_img)
+	// 	free(game_data->wall_img);
+	// if (game_data->ocean_img)
+	// 	free(game_data->ocean_img);
+	// if (game_data->player_img)
+	// 	free(game_data->player_img);
 	//if (game_data->exit_img)
 	//	free(game_data->map->lines);
-	// game_data->exit_img = NULL;
-	// game_data->fish_img = NULL;
-	// game_data->wall_img = NULL;
-	// game_data->ocean_img = NULL;
-	// game_data->player_img = NULL;
-	// free(game_data->mlx);
 	mlx_close_window(game_data->mlx);
-	// free(game_data->mlx);
-	// free(game_data);
 }
 
 void	error_msg(char *msg)
