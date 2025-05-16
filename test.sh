272b378d734e6ffaf1a1b769602d115ef86555cd
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-05-16 12:02:01 +0200

    feat: test laucher

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear && make
+printf "\nmaps/error-border-1.ber\n"
+./so_long maps/error-border-1.ber
+printf "\nmaps/error-border-2.ber\n"
+./so_long maps/error-border-2.ber
+printf "\nmaps/error-border-3.ber\n"
+./so_long maps/error-border-3.ber
+printf "\nmaps/error-border-4.ber\n"
+./so_long maps/error-border-4.ber
+printf "\nmaps/error-cant-exit.ber\n"
+./so_long maps/error-cant-exit.ber
+printf "\nmaps/error-exit-blocked.ber\n"
+./so_long maps/error-exit-blocked.ber
+printf "\nmaps/error-fish-blocked.ber\n"
+./so_long maps/error-fish-blocked.ber
+printf "\nmaps/error-no-exit.ber\n"
+./so_long maps/error-no-exit.ber
+printf "\nmaps/error-no-fish.ber\n"
+./so_long maps/error-no-fish.ber
+printf "\nmaps/error-no-player.ber\n"
+./so_long maps/error-no-player.ber
+printf "\nmaps/error-no-rectangle.ber\n"
+./so_long maps/error-no-rectangle.ber
+printf "\nmaps/error-player-blocked.ber\n"
+./so_long maps/error-player-blocked.ber
+printf "\nmaps/error-two-players.ber\n"
+./so_long maps/error-two-players.ber
+
