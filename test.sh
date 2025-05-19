ff26af8e430301a71132ce8924b4b783c5ebb7cc
Salvador Rioboo Naranjo <srioboo-@student.42malaga.com>
2025-05-19 16:41:48 +0200

    chore: add maps errors

--- test.sh
+++ test.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
 clear && make
-printf "\nmaps/error-border-1.ber\n"
+printf "\nBorder: maps/error-border-1.ber\n"
 ./so_long maps/error-border-1.ber
-printf "\nmaps/error-border-2.ber\n"
+printf "\nBorder: maps/error-border-2.ber\n"
 ./so_long maps/error-border-2.ber
-printf "\nmaps/error-border-3.ber\n"
+printf "\nBorder: maps/error-border-3.ber\n"
 ./so_long maps/error-border-3.ber
-printf "\nmaps/error-border-4.ber\n"
+printf "\nBorder: maps/error-border-4.ber\n"
 ./so_long maps/error-border-4.ber
-printf "\nmaps/error-cant-exit.ber\n"
+
+printf "\nExit: maps/error-cant-exit.ber\n"
 ./so_long maps/error-cant-exit.ber
-printf "\nmaps/error-exit-blocked.ber\n"
-./so_long maps/error-exit-blocked.ber
-printf "\nmaps/error-fish-blocked.ber\n"
-./so_long maps/error-fish-blocked.ber
-printf "\nmaps/error-no-exit.ber\n"
+printf "\nExit: maps/error-no-exit.ber\n"
 ./so_long maps/error-no-exit.ber
-printf "\nmaps/error-no-fish.ber\n"
+printf "\nExit: maps/error-exit-blocked.ber\n"
+./so_long maps/error-exit-blocked.ber
+
+printf "\nFish: maps/error-fish-blocked.ber\n"
+./so_long maps/error-fish-blocked.ber
+printf "\nFish: maps/error-no-fish.ber\n"
 ./so_long maps/error-no-fish.ber
-printf "\nmaps/error-no-player.ber\n"
+
+printf "\nPlayer: maps/error-no-player.ber\n"
 ./so_long maps/error-no-player.ber
-printf "\nmaps/error-no-rectangle.ber\n"
-./so_long maps/error-no-rectangle.ber
-printf "\nmaps/error-player-blocked.ber\n"
+printf "\nPlayer: maps/error-player-blocked.ber\n"
 ./so_long maps/error-player-blocked.ber
-printf "\nmaps/error-two-players.ber\n"
+printf "\nPlayermaps/error-two-players.ber\n"
 ./so_long maps/error-two-players.ber
 
+printf "\nShape: maps/error-no-rectangle.ber\n"
+./so_long maps/error-no-rectangle.ber
+
+printf "\nEmpty: maps/error-empty.ber"
+./so_long maps/error-empty.ber
+
+printf "\nExtension: maps/map.ber.wrong\n"
+./so_long maps/map.ber.wrong
